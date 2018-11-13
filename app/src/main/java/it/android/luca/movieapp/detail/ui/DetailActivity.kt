552409091bb93f9ace033ab8f3ff05c42993527b
Pierluca93 <luca@tengio.com>
2018-11-13 17:00:29 +0000

    removed unused import

--- app/src/main/java/it/android/luca/movieapp/detail/ui/DetailActivity.kt
+++ app/src/main/java/it/android/luca/movieapp/detail/ui/DetailActivity.kt
@@ -3,37 +3,21 @@ package it.android.luca.movieapp.detail.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.AppBarLayout
-import android.support.v4.content.ContextCompat.startActivity
-import android.text.TextUtils
-import android.view.View
-import android.view.View.*
 import com.bumptech.glide.Glide
 import it.android.luca.movieapp.App
 import it.android.luca.movieapp.BaseActivity
 import it.android.luca.movieapp.R
 import it.android.luca.movieapp.detail.presenter.DefaultDetailPresenter
 import it.android.luca.movieapp.di.DaggerDetailComponent
-import it.android.luca.movieapp.di.DaggerHomeComponent
 import it.android.luca.movieapp.di.DetailModule
-import it.android.luca.movieapp.di.HomeModule
-import it.android.luca.movieapp.home.presenter.DefaultHomePresenter
-import it.android.luca.movieapp.network.MovieApi
 import it.android.luca.movieapp.network.MovieApi.Companion.IMAGE_URL
 import it.android.luca.movieapp.repository.Movie
 import kotlinx.android.synthetic.main.activity_detail.*
-import kotlinx.android.synthetic.main.activity_detail.view.*
 import javax.inject.Inject
 import android.graphics.drawable.Drawable
-import com.squareup.picasso.Picasso
-import android.graphics.Bitmap
-import android.graphics.Color
 import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.ColorDrawable
 import com.bumptech.glide.request.target.ImageViewTarget
 import android.support.v7.graphics.Palette
-import android.provider.MediaStore.Images.Media.getBitmap
-
 
 
 
